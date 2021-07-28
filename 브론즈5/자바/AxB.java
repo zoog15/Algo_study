c7020a3ee37066a093ed4c5cc96959d47db31bfa
KimMinKwan <zoog15@naver.com>
2021-07-28 22:10:39 +0900

    Create AxB.java

---
+++ 브론즈5/자바/AxB.java
@@ -0,0 +1,16 @@
+package b5;
+
+import java.util.Scanner;
+
+/** 10998번 A*B */
+public class AxB {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a*b);
+		sc.close();
+	}
+}
