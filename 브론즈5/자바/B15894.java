3eec8fa7c378552fd7e5dc6f792563b11ab5971d
KimMinKwan <zoog15@naver.com>
2021-07-30 22:14:54 +0900

    Create B15894.java

---
+++ 브론즈5/자바/B15894.java
@@ -0,0 +1,16 @@
+package b5;
+
+import java.util.Scanner;
+
+/** 15894번 : 수학은 체육과목 입니다*/
+public class B15894 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		
+		System.out.println(n*4);
+		
+		sc.close();
+	}
+}
