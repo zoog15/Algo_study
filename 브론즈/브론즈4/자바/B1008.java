c9c41136620089b730a7768a51ce5d8726fd1e95
KimMinKwan <zoog15@naver.com>
2021-08-01 17:02:59 +0900

    디렉터리 변경

---
+++ 브론즈/브론즈4/자바/B1008.java
@@ -0,0 +1,15 @@
+package b4;
+
+import java.util.Scanner;
+
+/** 1008번 A/B */
+public class B1008 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		
+		System.out.println(a/b);
+	}
+}
---
+++ 브론즈/브론즈4/파이썬/1008번 A나누기B.py
@@ -0,0 +1,5 @@
+# 1008번 A/B : https://www.acmicpc.net/problem/1008
+
+a, b = map(int, input().split())
+
+print(a/b)
