1eeb24cdabf3447de58fa7f61cf66edbac96d70c
KimMinKwan <zoog15@naver.com>
2021-08-13 15:38:15 +0900

    기본 문제

---
+++ 코드업/파이썬 기초 100/6089 수 나열2.py
@@ -0,0 +1,6 @@
+a, b, c = map(int, input().split())
+
+for i in range(c-1):
+    a *= b
+
+print(a)
---
+++ 코드업/파이썬 기초 100/6090 수 나열3.py
@@ -0,0 +1,6 @@
+a, b, c, d = map(int, input().split())
+
+for i in range(d-1):
+    a = (a*b)+c
+
+print(a)
