8eb7189da29a220db1aa212b7fecebe01b30a76b
KimMinKwan <zoog15@naver.com>
2021-08-18 14:14:20 +0900

    2차원배열

---
+++ 코드업/2차원 배열/자바/S_1461.java
@@ -0,0 +1,28 @@
+package Array2;
+
+import java.util.Scanner;
+
+public class S_1461 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[][] arr = new int[n][n];
+		int num = 1;
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = n-1; j >=0; j--){
+				arr[i][j] = num++;
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++){
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
