ab959c7f65ac52ba18ea98d090495b457526e526
KimMinKwan <zoog15@naver.com>
2021-08-18 21:32:16 +0900

    2차원배열

---
+++ 코드업/2차원 배열/파이썬/1473 2차원 배열 지그재그 채우기2-6.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+a = [[0]*m for _ in range(n)]
+num = 1
+
+for i in range(n):
+    for j in range(m):
+        if i % 2 == 0:
+            a[n-1-i][j] = num
+        else:
+            a[n-1-i][m-1-j] = num
+        num += 1
+
+for i in range(n):
+    for j in range(m):
+        print(a[i][j], end=' ')
+    print()
