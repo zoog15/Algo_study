5717ca76f828404001afb3d8137586a584552142
KimMinKwan <zoog15@naver.com>
2021-08-18 14:16:31 +0900

    2차원배열

---
+++ 코드업/2차원 배열/파이썬/1462 2차원 배열 채우기1-3.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+a = [[0]*n for _ in range(n)]
+num = 1
+
+for i in range(n):
+    for j in range(n):
+        a[j][i] = num
+        num += 1
+
+for i in a:
+    for j in i:
+        print(j, end=' ')
+    print()
