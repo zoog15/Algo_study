a74626c96ac6419e5f3396bf22ee65c215cda779
KimMinKwan <zoog15@naver.com>
2021-08-18 21:10:25 +0900

    2차원배열

---
+++ 코드업/2차원 배열/파이썬/1467 2차원 배열 채우기1-8.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+a = [[0]*m for _ in range(n)]
+num = 1
+
+for i in range(m-1,-1,-1):
+    for j in range(n):
+        a[j][i] = num
+        num += 1
+
+for i in range(n):
+    for j in range(m):
+        print(a[i][j], end=' ')
+    print()
