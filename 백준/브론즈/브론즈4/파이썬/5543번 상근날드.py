af288a5320ed4d8ae522d6d1c47626ab77dfcb74
KimMinKwan <zoog15@naver.com>
2021-12-27 17:19:10 +0900

    5543번 상근날드

---
+++ 백준/브론즈/브론즈4/파이썬/5543번 상근날드.py
@@ -0,0 +1,13 @@
+burger = []
+drink = []
+
+for i in range(3):
+    burger.append(int(input()))
+
+for i in range(2):
+    drink.append(int(input()))
+
+burger.sort()
+drink.sort()
+
+print(burger[0]+drink[0]-50)
